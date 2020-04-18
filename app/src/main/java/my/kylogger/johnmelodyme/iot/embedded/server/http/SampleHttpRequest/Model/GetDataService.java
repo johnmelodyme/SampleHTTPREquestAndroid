d940553a117fe00bb34a4aa84bb262a6a5fe5a2e
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-19 02:51:26 +0800

    Create GetDataService.java

---
+++ app/src/main/java/my/kylogger/johnmelodyme/iot/embedded/server/http/SampleHttpRequest/Model/GetDataService.java
@@ -0,0 +1,12 @@
+package my.kylogger.johnmelodyme.iot.embedded.server.http.SampleHttpRequest.Model;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public class GetDataService {
+
+//    @GET("/photos")
+//    Call<List>
+}
